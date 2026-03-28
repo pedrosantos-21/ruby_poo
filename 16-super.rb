869a909650e77d091e7658234c4a8b05c4db2079
pedro vinicius <pedrovinipv58@gmail.com>
2026-03-28 19:10:50 -0300

    Estudando sobre o Super

---
+++ 16-super.rb
@@ -0,0 +1,60 @@
+class Franquia
+  def descricao
+    "Franquia"
+  end
+
+end
+
+class Franqueado < Franquia
+  def descricao
+    puts super # devolve o metodo da classe pai 
+    "Franqueado"
+  end
+end
+
+#########################
+
+
+f = Franquia.new
+puts f.descricao
+
+puts "=============================="
+
+ff = Franqueado.new
+puts ff.descricao
+
+puts "============================== \n"
+
+##########################
+
+class Conta
+  attr_accessor :numero, :saldo
+
+  def initialize(numero, saldo = 0)
+    @numero = numero
+    @saldo = saldo
+    
+  end
+end
+
+class ContaEpecial < Conta
+  attr_accessor :limite_especial
+
+  def initialize(numero, saldo, limite_especial)
+    super(numero, saldo) #chama o construtor da classe pai
+    @limite_especial = limite_especial
+  end
+
+end
+##########################
+
+c = Conta.new("001", 100.00)
+puts c.numero
+puts c.saldo
+
+puts "-----------------------------"
+
+ce = ContaEpecial.new("002", 200.00, 1000.00)
+puts ce.numero
+puts ce.saldo
+puts ce.limite_especial
