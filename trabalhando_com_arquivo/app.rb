da31359e452245b4c2e7fe1916ed67e977295f72
pedro vinicius <pedrovinipv58@gmail.com>
2026-04-11 12:13:14 -0300

    Trabalhando com ARGV

--- trabalhando_com_arquivo/app.rb
+++ trabalhando_com_arquivo/app.rb
@@ -1,7 +1,12 @@
 
-
-File.open('teste.txt', 'r') do |arq|
-  while line = arq.gets
-    puts line
+if ARGV.size > 0
+  File.open(ARGV[0], 'r') do |file|
+    while line = file.gets
+      puts line
+    end
   end
+else
+  puts "Nenhum arquivo especificado, especifique o nome do arquivo. Ex: ruby app.rb arquivo.txt"
 end
+
+#Lembrando, o app.rb é um espelho do que será executado, ou seja, o código do app.rb é o código que será executado.
