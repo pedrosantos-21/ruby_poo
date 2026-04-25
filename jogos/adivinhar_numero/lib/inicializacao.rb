836453dbb893db725a0031d7b132f0f34416506a
pedro vinicius <pedrovinipv58@gmail.com>
2026-04-25 16:25:34 -0300

    Executando comandos do SO

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,10 +1,12 @@
 class Inicializacao
   def self.inicializando
+    system ('cls')
     print "inicializando o jogo." 
     4.times do |i|
       sleep 1
       print "."
     end
     puts "."
+    system ('cls')
   end
 end
