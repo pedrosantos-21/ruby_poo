664dc8fbdfee256f3818349254fb265d3344eda8
pedro vinicius <pedrovinipv58@gmail.com>
2026-04-25 17:59:45 -0300

    Inserindo Gems estáticas no terminal

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,7 +1,19 @@
+require 'tty-cursor'
+require 'tty-spinner'
+
 class Inicializacao
   def self.inicializando
     system ('cls')
     
+    cursor = TTY::Cursor
+    print cursor.move_to(60, 15)
+    
+    spinner = TTY::Spinner.new
+    spinner = TTY::Spinner.new("[:spinner] Carregando...", format: :pulse_2)
+    spinner.auto_spin # Automatic animation with default interval
+    sleep(5) # Perform task
+    spinner.stop("Pronto!") # Stop animation
+
     #print "inicializando o jogo." 
     #4.times do |i|
     #  sleep 1
@@ -9,11 +21,11 @@ class Inicializacao
     #end
     #puts "."
 
-    File.open(File.expand_path('../../ascii/bat.txt',__FILE__), 'r') do |file|
-      while line = file.gets
-        puts line
-      end
-    end
+    #File.open(File.expand_path('../../ascii/bat.txt',__FILE__), 'r') do |file|
+    #  while line = file.gets
+    #    puts line
+    #  end
+    #end
 
     sleep 3
 
