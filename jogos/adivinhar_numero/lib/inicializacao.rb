aeaa680cad029320b305b51d6d5949a7897edb55
pedro vinicius <pedrovinipv58@gmail.com>
2026-04-25 17:16:43 -0300

    Brincando com o ASCII Art

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,12 +1,22 @@
 class Inicializacao
   def self.inicializando
     system ('cls')
-    print "inicializando o jogo." 
-    4.times do |i|
-      sleep 1
-      print "."
+    
+    #print "inicializando o jogo." 
+    #4.times do |i|
+    #  sleep 1
+    #  print "."
+    #end
+    #puts "."
+
+    File.open(File.expand_path('../../ascii/bat.txt',__FILE__), 'r') do |file|
+      while line = file.gets
+        puts line
+      end
     end
-    puts "."
+
+    sleep 3
+
     system ('cls')
   end
 end
